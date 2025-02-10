030d426a4ab21170c7723979b2a741959095efbd
Wojtek <returnpll@gmail.com>
2025-02-10 19:38:11 +0100

    codeforces10_02_25

--- cpp/B/21.cpp
+++ cpp/B/21.cpp
@@ -8,6 +8,34 @@ int main() {
     int a2, b2, c2;
     cin >> a2 >> b2 >> c2;
 
+    if (a1 == 0 && b1 == 0 && c1 == 0) {
+        if (a2 == 0 && b2 == 0 && c2 == 0) {
+            cout << -1;
+        } else if (a2 == 0 && b2 == 0 && c2 != 0) {
+            cout << 0;
+        } else {
+            cout << -1;
+        }
+        return 0;
+    }
+
+    if (a2 == 0 && b2 == 0 && c2 == 0) {
+        if (a1 == 0 && b1 == 0 && c1 == 0) {
+            cout << -1;
+        } else if (a1 == 0 && b1 == 0 && c1 != 0) {
+            cout << 0;
+        } else {
+            cout << -1;
+        }
+        return 0;
+    }
+
+    if (a1 == 0 && a2 == 0 && b1 == 0 && b2 == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+
     int d = a1 * b2 - a2 * b1;
 
     int d1 = -c1 * b2 + c2 * b1;
